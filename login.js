c6cc3aed86d399390a8a0f0fc6f622bfcca94dee
nhasachcaibe <nhasachcaibe@gmail.com>
2026-06-23 00:30:22 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,14 @@
+function loginAdmin(){
+  const pass = document.getElementById("adminPass").value.trim();
+
+  // Đổi mật khẩu này theo ý bạn
+  const ADMIN_PASSWORD = "123456";
+
+  if(pass === ADMIN_PASSWORD){
+    localStorage.setItem("admin_logged_in", "yes");
+    location.href = "admin.html";
+  }else{
+    document.getElementById("msg").innerHTML =
+      "<p style='color:red'>Sai mật khẩu admin.</p>";
+  }
+}
